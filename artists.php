2572fc58b47932af88785f020065bc290574ebc1
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-02 19:25:22 -0600

    Update artists.php

--- artists.php
+++ artists.php
@@ -1,8 +1,11 @@
 <?php
-$pageTitle = "Home";
+require_once("util-db.php");
+require_once("model-artists.php");
+
+$pageTitle = "Artists";
 include "view-header.php";
-?>
-<h1>Homework 3</h1>
-<?php
+$artists = selectArtists();
+include "view-artists.php";
 include "view-footer.php";
+//this is the control for artists and we're creating the view and model files
 ?>
