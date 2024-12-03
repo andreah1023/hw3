cca2d252ab88583bec73116dbb6f15365d736fdc
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-02 20:28:55 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('138.197.17.168', 'andreaph_hw3user', 'Vb[B}~.z](JP', 'andreaph_hw3');
+    $conn = new mysqli('138.197.17.168', 'andreaph_hw3user', 'Vb[B}~.z](JP', 'andreapho_hw3');
     
     // Check connection
     if ($conn->connect_error) {
