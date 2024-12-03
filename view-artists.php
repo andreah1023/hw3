af1ad016963e89d4501b91b832cf4b6ed78e3793
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-02 20:01:29 -0600

    Create view-artists.php

---
+++ view-artists.php
@@ -0,0 +1,25 @@
+<h1>Artists</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Room</th>     
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($artist = $artists->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $artist['artist_id']; ?></td>
+    <td><?php echo $artist['artist_name']; ?></td>
+    <td><?php echo $artist['room_number']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
