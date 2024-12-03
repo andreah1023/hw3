e8474424440632c5178f7480586721fc6584f385
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-03 12:48:48 -0600

    Create view-albums.php

---
+++ view-albums.php
@@ -0,0 +1,25 @@
+<h1>Albums</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Number</th>
+      <th>Name</th>     
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($album = $albums->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $album['album_id']; ?></td>
+    <td><?php echo $album['album_number']; ?></td>
+    <td><?php echo $artist['album_name']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
