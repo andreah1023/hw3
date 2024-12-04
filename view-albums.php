276888ce9c462ce243850d6e621f0fbd640ac316
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-03 22:03:11 -0600

    Update view-albums.php

--- view-albums.php
+++ view-albums.php
@@ -5,7 +5,8 @@
       <tr>
       <th>ID</th>
       <th>Number</th>
-      <th>Name</th>     
+      <th>Name</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -16,6 +17,12 @@ while ($album = $albums->fetch_assoc()) {
     <td><?php echo $album['album_id']; ?></td>
     <td><?php echo $album['album_number']; ?></td>
     <td><?php echo $album['album_name']; ?></td>
+    <td>
+      <form method="post" action="songs-by-album.php">
+        <input type="hidden" name="alid" value="<?php echo $album['album_id']; ?>">
+        <button type="submit" class="btn btn-primary">Songs</button>
+      </form>
+    </td>
   </tr>
 <?php
 }
