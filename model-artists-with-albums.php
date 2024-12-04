bd6d240c21d566eabe23854b9e5c89164970adf1
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-03 22:56:01 -0600

    Update model-artists-with-albums.php

--- model-artists-with-albums.php
+++ model-artists-with-albums.php
@@ -12,4 +12,19 @@ function selectArtists() {
         throw $e;
     }
 }
+
+function selectAlbumsByArtist($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT a.album_id, album_number, album_name, release_year, room FROM `album` a join song s on s.album_id = a.album_id where s.artist_id=?");
+        $stmt->bind_param("i", $aid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
