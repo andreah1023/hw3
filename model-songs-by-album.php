a086ab3b0bf33541e86848be0062abf268005592
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-03 21:36:20 -0600

    Update model-songs-by-album.php

--- model-songs-by-album.php
+++ model-songs-by-album.php
@@ -2,8 +2,8 @@
 function selectSongsByAlbum($alid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT a.album_id, album_number, album_name, release_year, room FROM `album` a join song s on s.album_id = a.album_id where s.artist_id=?");
-        $stmt->bind_param("i", $aid);
+        $stmt = $conn->prepare("SELECT a.album_id, album_number, album_name, release_year, room FROM `album` a join song s on s.album_id = a.album_id where s.album_id=?");
+        $stmt->bind_param("i", $alid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
