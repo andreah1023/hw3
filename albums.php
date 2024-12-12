766915d1d5915295a91acafc29b94e4993561aa5
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-12 16:30:24 -0600

    Update albums.php

--- albums.php
+++ albums.php
@@ -4,6 +4,15 @@ require_once("model-albums.php");
 
 $pageTitle = "Albums";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertAlbums($_POST['aNumber'], $_POST['aName']);
+      break;
+  }
+}
+  
 $albums = selectAlbums();
 include "view-albums.php";
 include "view-footer.php";
