7428b0adedf6e28f9296216a31cd16888c6ff7ec
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-12 16:37:14 -0600

    Update albums.php

--- albums.php
+++ albums.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertAlbums($_POST['aNumber'], $_POST['aName']);
+      If (insertAlbums($_POST['aNumber'], $_POST['aName'])) {
+        echo '<div class="alert alert-success" role="alert">Course added.</div>"';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>"';
+      }
       break;
   }
 }
