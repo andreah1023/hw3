3b2c1412357c3948f5b0dd9394a878581084a64d
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-12 15:26:14 -0600

    Update model-albums.php

--- model-albums.php
+++ model-albums.php
@@ -12,4 +12,46 @@ function selectAlbums() {
         throw $e;
     }
 }
+
+function insertAlbums($aNumber, $aName) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `album` (`album_number`, `album_name`) VALUES (?, ?)";
+        $stmt->bind_param("ss", $aNumber, $aName);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateAlbums($aNumber, $aName, $aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `album` set `album_number` = ?, `album_name` = ? where album_id = ?");
+        $stmt->bind_param("ssi", $aNumber, $aName, $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteAlbums($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from album where album_id=?");
+        $stmt->bind_param("i", $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
