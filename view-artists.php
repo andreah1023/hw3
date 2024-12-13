b2ff71847d5c7cd90f3c0e12cef44c6cb109f0e3
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-13 03:32:58 -0600

    Update view-artists.php

--- view-artists.php
+++ view-artists.php
@@ -1,27 +1,27 @@
-<h1>Artists</h1>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Room</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-<?php
-while ($artist = $artists->fetch_assoc()) {
-?>
-  <tr>
-    <td><?php echo $artist['artist_id']; ?></td>
-    <td><?php echo $artist['artist_name']; ?></td>
-    <td><?php echo $artist['room_number']; ?></td>
-    <td><a href="albums-by-artist.php?id=<?php echo $artist['artist_id']; ?>">Albums</a></td>
-  </tr>
-<?php
-}
-?>
-    </tbody>
-  </table>
+<div class="container-fluid" style="background-color: #ff4d4d; padding: 20px; border-radius: 8px;">
+  <h1 style="color: #ffffff; font-weight: bold; text-align: center;">Artists</h1>
+  <div class="table-responsive">
+    <table class="table table-bordered table-striped">
+      <thead style="background-color: #ffcccc; color: #800000;">
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Room</th>
+          <th>Albums</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php while ($artist = $artists->fetch_assoc()) { ?>
+          <tr style="color: #ffffff;">
+            <td><?php echo $artist['artist_id']; ?></td>
+            <td><?php echo $artist['artist_name']; ?></td>
+            <td><?php echo $artist['room_number']; ?></td>
+            <td>
+              <a href="albums-by-artist.php?id=<?php echo $artist['artist_id']; ?>" class="btn btn-light btn-sm">View Albums</a>
+            </td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
 </div>
