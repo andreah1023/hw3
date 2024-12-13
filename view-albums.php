85dae2676c80db0b0f698afdfc1a99804edc6a44
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-13 03:39:01 -0600

    Update view-albums.php

--- view-albums.php
+++ view-albums.php
@@ -1,80 +1,60 @@
-<div class="row mb-3">
+<div class="row">
   <div class="col">
-    <h1>Albums</h1>
+<h1>Albums</h1>
   </div>
   <div class="col-auto">
-    <?php include "view-albums-newform.php"; ?>
+<?php
+include "view-albums-newform.php";
+?>
   </div>
-</div>
-
-<!-- Dropdown to filter albums -->
-<div class="row mb-3">
-  <div class="col">
-    <form method="get" action="">
-      <label for="filter" class="form-label">Filter by Artist:</label>
-      <select name="artist_id" id="filter" class="form-select" onchange="this.form.submit()">
-        <option value="">All Artists</option>
-        <?php
-        // Fetch artists for the dropdown
-        $artists = getArtists(); // Replace with your function to fetch artists
-        while ($artist = $artists->fetch_assoc()) {
-          $selected = ($_GET['artist_id'] ?? '') == $artist['artist_id'] ? 'selected' : '';
-          echo "<option value='{$artist['artist_id']}' $selected>{$artist['artist_name']}</option>";
-        }
-        ?>
-      </select>
-    </form>
-  </div>
-</div>
-
-<!-- Album Table -->
+</div> 
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
-        <th>ID</th>
-        <th>Number</th>
-        <th>Name</th>
+      <th>ID</th>
+      <th>Number</th>
+      <th>Name</th>
         <th></th>
         <th></th>
         <th></th>
       </tr>
     </thead>
     <tbody>
-      <?php
-      // Filter albums by selected artist
-      $selectedArtistId = $_GET['artist_id'] ?? null;
-      $albums = $selectedArtistId ? getAlbumsByArtist($selectedArtistId) : getAllAlbums(); // Replace with your query logic
-      
-      while ($album = $albums->fetch_assoc()) {
-      ?>
-        <tr>
-          <td><?php echo $album['album_id']; ?></td>
-          <td><?php echo $album['album_number']; ?></td>
-          <td><?php echo $album['album_name']; ?></td>
-          <td>
-            <?php include "view-albums-editform.php"; ?>
-          </td>
-          <td>
-            <form method="post" action="">
-              <input type="hidden" name="alid" value="<?php echo $album['album_id']; ?>">
-              <input type="hidden" name="actionType" value="Delete">
-              <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1 1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                </svg>
-              </button>
-            </form>
-          </td>
-          <td>
-            <form method="post" action="songs-by-album.php">
-              <input type="hidden" name="alid" value="<?php echo $album['album_id']; ?>">
-              <button type="submit" class="btn btn-primary">Songs</button>
-            </form>
-          </td>
-        </tr>
-      <?php } ?>
+<?php
+while ($album = $albums->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $album['album_id']; ?></td>
+    <td><?php echo $album['album_number']; ?></td>
+    <td><?php echo $album['album_name']; ?></td>
+    <td>
+<?php
+include "view-albums-editform.php";
+?>
+    </td>
+    <td>
+      <form method="post" action="">
+      <input type="hidden" name="alid" value="<?php echo $album['album_id']; ?>">
+      <input type="hidden" name="actionType" value="Delete">
+      <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+      </svg>
+      </button>
+      </form>
+    </td>
+    <td>
+      <form method="post" action="songs-by-album.php">
+        <input type="hidden" name="alid" value="<?php echo $album['album_id']; ?>">
+        <button type="submit" class="btn btn-primary">Songs</button>
+      </form>
+    </td>
+  </tr>
+<?php
+}
+?>
     </tbody>
   </table>
 </div>
