c2f87d004d31118e21592ee279b3128840fb520f
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-13 03:04:50 -0600

    Update artists.php

--- artists.php
+++ artists.php
@@ -9,23 +9,23 @@ if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add": 
       if (insertArtist($_POST['aName'],$_POST['aNumber'])) {
-      echo '<div class="alert alert-success" role="alert"> A doctor has been added!</div>';
+      echo '<div class="alert alert-success" role="alert"> A artist has been added!</div>';
       } else {
       echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
       }
       break;
 
      case "Edit": 
-      if (updateDoctor($_POST['dName'],$_POST['dSpecialization'],$_POST['did'])) {
-      echo '<div class="alert alert-success" role="alert"> A doctor has been edited!</div>';
+      if (updateArtist($_POST['aName'],$_POST['aNumber'],$_POST['alid'])) {
+      echo '<div class="alert alert-success" role="alert"> A artist has been edited!</div>';
       } else {
       echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
       }
       break;
     
       case "Delete": 
-      if (deleteDoctor($_POST['did'])) {
-      echo '<div class="alert alert-success" role="alert"> A doctor has been deleted!</div>';
+      if (deleteArtist($_POST['alid'])) {
+      echo '<div class="alert alert-success" role="alert"> A artist has been deleted!</div>';
       } else {
       echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
       }
