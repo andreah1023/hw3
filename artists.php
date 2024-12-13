b69b9e67f7cc2ced2adf11293846ac067429ed3b
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-13 03:02:09 -0600

    Update artists.php

--- artists.php
+++ artists.php
@@ -4,8 +4,37 @@ require_once("model-artists.php");
 
 $pageTitle = "Artists";
 include "view-header.php";
-$artists = selectArtists();
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add": 
+      if (insertArtist($_POST['aName'],$_POST['aNumber'])) {
+      echo '<div class="alert alert-success" role="alert"> A doctor has been added!</div>';
+      } else {
+      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+      }
+      break;
+
+     case "Edit": 
+      if (updateDoctor($_POST['dName'],$_POST['dSpecialization'],$_POST['did'])) {
+      echo '<div class="alert alert-success" role="alert"> A doctor has been edited!</div>';
+      } else {
+      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+      }
+      break;
+    
+      case "Delete": 
+      if (deleteDoctor($_POST['did'])) {
+      echo '<div class="alert alert-success" role="alert"> A doctor has been deleted!</div>';
+      } else {
+      echo '<div class="alert alert-danger" role="alert"> Error!</div>'; 
+      }
+      break;
+   
+  }
+}
+
+$artists=selectArtists();
 include "view-artists.php";
 include "view-footer.php";
-//this is the control for artists and we're creating the view and model files
 ?>
