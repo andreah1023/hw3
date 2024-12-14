e1decf714190e801b2ebae427fecd0cf922c761e
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-13 21:48:31 -0600

    Update artists-with-albums.php

--- artists-with-albums.php
+++ artists-with-albums.php
@@ -4,6 +4,20 @@ require_once("model-artists-with-albums.php");
 
 $pageTitle = "Artists with Albums";
 include "view-header.php";
+
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      If (insertSongs($_POST['aid'], $_POST['alid'], $_POST['sYear'], $_POST['sRoom'])) {
+        echo '<div class="alert alert-success" role="alert">Song added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
 $artists = selectArtists();
 include "view-artists-with-albums.php";
 include "view-footer.php";
