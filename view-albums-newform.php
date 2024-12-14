d143ad68389796ef7549e92f79e041cab53cd4a2
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-13 20:39:24 -0600

    Update view-albums-newform.php

--- view-albums-newform.php
+++ view-albums-newform.php
@@ -15,21 +15,16 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form>
+        <form method="post" action="">
           <div class="mb-3">
-            <label for="aNumber" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <label for="alNumber" class="form-label">Album number</label>
+            <input type="text" class="form-control" id="alNumber" name="alNumber">
           </div>
           <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <label for="alName" class="form-label">Album name</label>
+            <input type="text" class="form-control" id="alName" name="alName">
           </div>
-          <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" class="btn btn-primary">Save</button>
         </form>
       </div>
     </div>
