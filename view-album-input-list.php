ca77f5d0d603ba37e62921893210277211961593
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-14 00:00:37 -0600

    Update view-album-input-list.php

--- view-album-input-list.php
+++ view-album-input-list.php
@@ -1,9 +1,6 @@
 <select class="form-select" id="alid" name="alid" >
 <?php
 while ($albumItem = $albumList->fetch_assoc()) {
-  $selText = "";
-  if($selectedAlbum == $AlbumItem['album_id']) {
-    $selText = " selected";
 ?>
   <option value="<?php echo $albumItem['album_id']; ?>"<?=$selText?>><?php echo $albumItem['album_name']; ?></option>
 <?php 
