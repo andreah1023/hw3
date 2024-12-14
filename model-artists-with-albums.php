2aa90ec75d1bdd5b67b1988a68015a43c4a3504d
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-13 21:50:39 -0600

    Update model-artists-with-albums.php

--- model-artists-with-albums.php
+++ model-artists-with-albums.php
@@ -16,7 +16,7 @@ function selectArtists() {
 function selectAlbumsByArtist($aid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT a.album_id, album_number, album_name, release_year, room FROM `album` a join song s on s.album_id = a.album_id where s.artist_id=?");
+        $stmt = $conn->prepare("SELECT album_id, album_number, album_name, release_year, room FROM `album` a join song s on s.album_id = a.album_id where s.artist_id=?");
         $stmt->bind_param("i", $aid);
         $stmt->execute();
         $result = $stmt->get_result();
