99db1f82ebf8720f902a88e79386365da8944f46
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-13 19:53:51 -0600

    Update model-artists-with-albums.php

--- model-artists-with-albums.php
+++ model-artists-with-albums.php
@@ -28,14 +28,44 @@ function selectAlbumsByArtist($aid) {
     }
 }
 
-function insertSongs() {
+function insertSongs($aid, $alid, $sYear, $sRoom) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT artist_id, artist_name, room_number FROM `artist`");
-        $stmt->execute();
-        $result = $stmt->get_result();
+        $stmt = $conn->prepare("INSERT INTO `song` (`artist_id`, `album_id`, `release_year`, `room`) VALUES ('2', '4', '2015', '2334')");
+        $stmt->bind_param("iiss", $aid, $alid, $sYear, $sRoom);
+        $success = $stmt->execute();
         $conn->close();
-        return $result;
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateSongs($aid, $alid, $sYear, $sRoom, $sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `song` set `artist_id` = ?, 'album_id' = ?, `release_year` = ?, 'room' = ? where song_id = ?");
+        $stmt->bind_param("iissi", $aid, $alid, $sYear, $sRoom, $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteSongs($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from song where song_id=?");
+        $stmt->bind_param("i", $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
