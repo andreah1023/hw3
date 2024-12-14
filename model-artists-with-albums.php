00968606868de134bd62a3a9765bb42974c3124c
andreah1023 <157570350+andreah1023@users.noreply.github.com>
2024-12-13 19:41:20 -0600

    Update model-artists-with-albums.php

--- model-artists-with-albums.php
+++ model-artists-with-albums.php
@@ -27,4 +27,19 @@ function selectAlbumsByArtist($aid) {
         throw $e;
     }
 }
+
+function insertSongs() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT artist_id, artist_name, room_number FROM `artist`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
